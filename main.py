25fcbd83de44bff35dda428a8aa6d27529c90ff6
Mark Peters <mapeters101@earthlink.net>
2024-09-26 19:14:46 -0400

    Lesson004: Add Fritz to calculations.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ for pirate in pirates:
         sum_of_ranks += 5
     elif pirate == "Isabel" or pirate == "Bootstrap Bill" or pirate == "Morgan":
         sum_of_ranks += 3
-    elif pirate == "Powder Joe":
+    elif pirate == "Powder Joe" or pirate == "Four Finger Fritz":
         sum_of_ranks += 2
 
 for pirate in pirates:
@@ -17,6 +17,6 @@ for pirate in pirates:
         share = 5 / sum_of_ranks * ducats
     elif pirate == "Isabel" or pirate == "Bootstrap Bill" or pirate == "Morgan":
         share = 3 / sum_of_ranks * ducats
-    elif pirate == "Powder Joe":
+    elif pirate == "Powder Joe" or pirate == "Four Finger Fritz":
         share = 2 / sum_of_ranks * ducats
     print(f"{pirate} gets {share} Ducats.")
