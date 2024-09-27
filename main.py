2d2008ae61aaa0be376acde29fe4d712dab790f7
Mark Peters <mapeters101@earthlink.net>
2024-09-26 23:55:16 -0400

    Lesson004: Format numbers.

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ sum_of_ranks = sum(rank for name, rank in pirates)
 
 for name, rank in pirates:
     share = rank / sum_of_ranks * ducats
-    print(f"{name} gets {share} Ducats.")
+    print(f"{name} gets {share:.2f} Ducats.")
