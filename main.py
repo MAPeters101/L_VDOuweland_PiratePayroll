d5662d4132301fcbd03b616a28734d26790aa2a3
Mark Peters <mapeters101@earthlink.net>
2024-09-27 15:40:51 -0400

    Lesson009: Refactor: Move pirate classes to pirates.py and import into main.py

--- main.py
+++ main.py
@@ -1,23 +1,7 @@
-class Pirate:
-    def __init__(self, name):
-        self.name = name
-
-class Captain(Pirate):
-    title = "Captain"
-    rank = 10
-
-class Quartermaster(Pirate):
-    title = "Quartermaster"
-    rank = 9
-
-class Officer(Pirate):
-    title = "Officer"
-    rank = 7
-
-class CannonOperator(Pirate):
-    title = "Cannon Operator"
-    rank = 6
-
+from pirates import Captain
+from pirates import Quartermaster
+from pirates import Officer
+from pirates import CannonOperator
 
 pirates = [
     Captain("Harry"),
