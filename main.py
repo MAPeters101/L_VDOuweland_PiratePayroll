1412a30234a0e47e5317ee83cbd6f0bc785a3668
Mark Peters <mapeters101@earthlink.net>
2024-09-26 23:58:47 -0400

    Lesson004: Update ranks.

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 pirates = [
-    ("Harry", "Captain", 5),
-    ("Isabel", "Quatermaster", 3),
-    ("Bootstrap Bill", "Mate", 3),
-    ("Powder Joe", "Gunner",2),
-    ("Four Finger Fritz", "Mate", 2)
+    ("Harry", "Captain", 10),
+    ("Isabel", "Quatermaster", 9),
+    ("Bootstrap Bill", "Mate", 7),
+    ("Powder Joe", "Gunner", 6),
+    ("Four Finger Fritz", "Mate", 7)
 ]
 ducats = 850
 sum_of_ranks = sum(rank for name, title, rank in pirates)
