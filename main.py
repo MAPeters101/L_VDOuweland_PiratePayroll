197ae5e193831db4f4c2edf1f97f7b0eadf2c839
Mark Peters <mapeters101@earthlink.net>
2024-09-26 23:57:36 -0400

    Lesson004: Add titles.

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 pirates = [
-    ("Harry", 5),
-    ("Isabel", 3),
-    ("Bootstrap Bill", 3),
-    ("Powder Joe", 2),
-    ("Four Finger Fritz", 2)
+    ("Harry", "Captain", 5),
+    ("Isabel", "Quatermaster", 3),
+    ("Bootstrap Bill", "Mate", 3),
+    ("Powder Joe", "Gunner",2),
+    ("Four Finger Fritz", "Mate", 2)
 ]
 ducats = 850
-sum_of_ranks = sum(rank for name, rank in pirates)
+sum_of_ranks = sum(rank for name, title, rank in pirates)
 
-for name, rank in pirates:
+for name, title, rank in pirates:
     share = rank / sum_of_ranks * ducats
-    print(f"{name} gets {share:.2f} Ducats.")
+    print(f"{title} {name} gets {share:.2f} Ducats.")
