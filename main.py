8b1d008308b3c13544e59b4159a86084271905af
Mark Peters <mapeters101@earthlink.net>
2024-09-28 14:41:21 -0400

    Lesson015: Refactor- Let Share class create the string:  Add __str__ method to Share class.  Change print statement in main.py to print share.

--- main.py
+++ main.py
@@ -12,5 +12,5 @@ payroll = Payroll()
 shares = payroll.calculate_shares(pirates, ducats)
 
 for share in shares:
-    print(f"{share.pirate.role.title} {share.pirate.name}, rank {share.pirate.role.rank} gets share {share.ducats:.2f} Ducats.")
+    print(share)
 
--- payroll.py
+++ payroll.py
@@ -3,6 +3,9 @@ class Share:
         self.pirate = pirate
         self.ducats = ducats
 
+    def __str__(self):
+        return f"{self.pirate.role.title} {self.pirate.name}, rank {self.pirate.role.rank} gets share {self.ducats:.2f} Ducats."
+
 class Payroll:
     def calculate_shares(self, crew, ducats):
         sum_of_ranks = sum(pirate.role.rank for pirate in crew)
