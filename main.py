4f739817c4146ade418c42f8f624e1278850175e
Mark Peters <mapeters101@earthlink.net>
2024-09-29 15:41:31 -0400

    Lesson018: Exchange loot other than ducats:  Create Bank class in exchange.py.  Create exchange method in Bank class.  Import Bank in main.py.  Create instance of Bank.  Convert loot to ducats in main.py.  Uses ducats instead of mission loot for shares calculation.

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from data import TestDataLoader
 from data import JSONDataLoader
 from payroll import Payroll
+from exchange import Bank
 
-# loader = TestDataLoader()
-loader = JSONDataLoader()
+loader = TestDataLoader()
+# loader = JSONDataLoader()
 
+bank = Bank(loader)
 payroll = Payroll()
 missions = loader.load_missions()
 for mission in missions:
     print(mission)
-    shares = payroll.calculate_shares(mission.crew, mission.loot)
+    ducats = bank.exchange(mission.loot)
+    print(f"Loot exchanged for {ducats} Ducats.")
+    shares = payroll.calculate_shares(mission.crew, ducats)
     for share in shares:
         print(share)
     print()
